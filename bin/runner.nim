a54cdd2025e4c56edce35dfbbadc349a7fa272c2
Saverio Trioni <saverio.trioni@gmail.com>
2021-11-11 13:17:14 +0100

    put nim runner template into a nim file

---
+++ bin/runner.nim
@@ -0,0 +1,7 @@
+#[ This file is not meant to be run by itself ]#
+
+import src/y__YEAR__/d__DAY__
+import src/utils/pretty
+
+let data = readFile("inputs/__YEAR__/__DAY__.txt")
+print(__YEAR__, __DAY__, $(𝟙(data)), $(𝟚(data)))
--- src/main.nim
+++
@@ -1,8 +0,0 @@
-import y2020/d02
-
-let data = readFile("inputs/2020/02.txt")
-
-echo data
-
-echo "Part 1: ", 𝟙(data)
-echo "Part 2: ", 𝟚(data)
