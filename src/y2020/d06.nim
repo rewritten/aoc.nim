8533e06219e94198c6304b59decc7e60b4726787
Saverio Trioni <saverio.trioni@gmail.com>
2021-11-13 10:19:13 +0100

    unloop a condition

--- src/y2020/d06.nim
+++ src/y2020/d06.nim
@@ -7,8 +7,8 @@ import std/strutils
 proc 𝟙*(data: string): int =
   for group in data.split("\n\n"):
     var ans: set[char] = {}
-    for c in group:
-      if c != '\n': ans.incl c
+    for c in group: ans.incl c
+    ans.excl '\n'
     result += len(ans)
 
 proc 𝟚*(data: string): int =
